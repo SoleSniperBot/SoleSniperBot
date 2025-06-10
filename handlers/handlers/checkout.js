be6b560470ade7e4d8cfa5693611a239ee0da1a4
SoleSniperBot <dniles-francis@hotmail.com>
2025-06-10 23:41:36 +0100

    Delete handlers directory

--- handlers/handlers/checkout.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function handleCheckout(ctx) {
-  ctx.reply(
-    '🛒 Let’s get you set up for Auto-Checkout!\n\n' +
-    'Use /addaccount to connect Nike, JD, etc.\n' +
-    'Use /addprofile to save your address & card info.\n\n' +
-    'Once ready, you’ll be able to snipe releases using your profiles and proxies 💸'
-  );
-};
