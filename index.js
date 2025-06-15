2e26aba5a3d464351dfa70b1ff3e5281394759a6
SoleSniperBot <dniles-francis@hotmail.com>
2025-06-15 10:43:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// index.js
 const express = require('express');
 const { Telegraf } = require('telegraf');
 const bodyParser = require('body-parser');
@@ -8,23 +7,22 @@ const setupAuth = require('./handlers/auth');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const app = express();
 
-app.use(bodyParser.json());
+// Stripe Webhook
+app.post('/webhook', bodyParser.raw({ type: 'application/json' }), webhookHandler, initWebhook(bot));
 
-// Stripe webhook
-app.post('/webhook', webhookHandler, initWebhook(bot));
-
-// Telegram webhook
+// Telegram Webhook
 const domain = process.env.DOMAIN;
 if (!domain) {
-  throw new Error('❌ DOMAIN environment variable not set!');
+  throw new Error('❌ DOMAIN environment variable not set');
 }
 
 bot.telegram.setWebhook(`${domain}/bot${process.env.BOT_TOKEN}`);
 app.use(bot.webhookCallback(`/bot${process.env.BOT_TOKEN}`));
 
-// Load commands
+// Bot Features
 setupAuth(bot);
 
+// Start Server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
