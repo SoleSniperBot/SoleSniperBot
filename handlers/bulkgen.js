8e1cf249d07e947844279946814c98ceec36db90
SoleSniperBot <dniles-francis@hotmail.com>
2025-06-19 00:40:13 +0100

    Update bulkgen.js

--- handlers/bulkgen.js
+++ handlers/bulkgen.js
@@ -1,4 +1,4 @@
-const generateNikeAccount = require('../lib/accountGenerator');
+const accountGenerator = require('./accountGenerator');
 
 module.exports = (bot) => {
   bot.command('bulkgen', async (ctx) => {
