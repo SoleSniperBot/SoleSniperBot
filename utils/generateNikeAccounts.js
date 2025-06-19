696faecbc21a146d51155b4722cfc7f83aafa0b1
SoleSniperBot <dniles-francis@hotmail.com>
2025-06-19 20:25:57 +0100

    Delete utils/generateNikeAccounts.js

--- utils/generateNikeAccounts.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function generateNikeAccount() {
-  // Placeholder logic for Nike account creation
-  return {
-    email: `user${Date.now()}@gmail.com`,
-    password: `TempPass!${Math.floor(Math.random() * 10000)}`
-  };
-};
