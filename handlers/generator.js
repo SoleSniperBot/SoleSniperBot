52270321c2afc009415f92e05b4d830d98b6b899
SoleSniperBot <dniles-francis@hotmail.com>
2025-07-01 22:56:14 +0100

    Create generator.js

---
+++ handlers/generator.js
@@ -0,0 +1,10 @@
+// lib/generator.js
+module.exports = async function generateNikeAccount(proxy) {
+  const timestamp = Date.now();
+  const randomNum = Math.floor(Math.random() * 10000);
+  const email = `solesniper+${timestamp}@gmail.com`;
+  const password = `TempPass!${randomNum}`;
+
+  console.log(`👟 Generated account with proxy: ${proxy}`);
+  return { email, password, proxy };
+};
