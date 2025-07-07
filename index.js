51b4adf0a621226285dbd44f91ae62dbed386c48
SoleSniperBot <dniles-francis@hotmail.com>
2025-07-08 00:08:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ require('./handlers/menu')(bot);
 require('./handlers/myaccounts')(bot);
 require('./handlers/rotateinline')(bot);
 require('./handlers/cooktracker')(bot);
-require('./handlers/generator')(bot);
+require('./handlers/gen')(bot);
 // JD profile selector inline buttons
 const { handleJDProfileSelection } = require('./handlers/jdcheckout');
 handleJDProfileSelection(bot);
