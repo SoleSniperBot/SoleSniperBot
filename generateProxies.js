6a95611e42d2141cf7c37993861847fd9483d7fb
SoleSniperBot <dniles-francis@hotmail.com>
2025-07-20 23:04:58 +0100

    Create generateProxies.js

---
+++ generateProxies.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, '../data/proxies.json');
+const user = process.env.GEONODE_USER;
+const pass = process.env.GEONODE_PASS;
+const host = process.env.GEONODE_HOST || 'proxy.geonode.io';
+const port = '9000'; // or another if custom
+
+const proxies = [];
+
+for (let i = 0; i < 50; i++) {
+  proxies.push({
+    formatted: `http://${user}:${pass}@${host}:${port}`
+  });
+}
+
+fs.writeFileSync(filePath, JSON.stringify(proxies, null, 2));
